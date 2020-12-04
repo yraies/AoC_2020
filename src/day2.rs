3f1e1caa317ca2645376391dc257ca1441f28c8c
Yasin Raies <yasin.raies@stud-mail.uni-wuerzburg.de>
2020-12-04 01:53:01 +0100

    Pest tests on Day 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,44 +1,91 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
-pub struct Policy {
+use pest_derive::Parser;
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "day2.pest"]
+pub struct PwParser;
+
+pub struct Password {
     c: char,
     v1: usize,
     v2: usize,
+    p: String,
+}
+
+#[aoc_generator(day2, alt=pest)]
+pub fn generate_policy_pairs_pest(input: &str) -> Vec<Password> {
+    let pairs = PwParser::parse(Rule::main, input).unwrap_or_else(|e| panic!("{}", e));
+    pairs.filter_map(|p| match p.as_rule() {
+        Rule::line => {
+            let mut line = p.into_inner();
+            Some(Password{
+                v1: line.next().unwrap().as_str().parse::<usize>().unwrap(),
+                v2: line.next().unwrap().as_str().parse::<usize>().unwrap(),
+                c: line.next().unwrap().as_str().parse::<char>().unwrap(),
+                p: line.next().unwrap().to_string(),
+            })
+        }
+        _ => None,
+    }).collect()
 }
 
 #[aoc_generator(day2)]
-pub fn generate_policy_pairs(input: &str) -> Vec<(Policy, String)> {
+pub fn generate_policy_pairs(input: &str) -> Vec<Password> {
     input.split("\n").map(|v| {
         let splits: Vec<&str> = v.split_whitespace().collect();
         let bounds: Vec<usize> = splits[0].split("-").map(|s| s.parse::<usize>().unwrap()).collect();
         let c = splits[1].chars().next().unwrap();
-        let policy = Policy { c, v1: bounds[0], v2: bounds[1] };
-        (policy, splits[2].to_string())
+        Password { c, v1: bounds[0], v2: bounds[1], p: splits[2].to_string() }
     }).collect()
 }
 
 #[aoc(day2, part1)]
-pub fn num_of_valid_passwords_oldplace(values: &[(Policy, String)]) -> usize {
-    values.iter().filter(|(policy, password)| {
-        let count = password.chars().filter(|&c| char::eq(&c, &policy.c)).count();
-        policy.v1 <= count && count <= policy.v2
+pub fn num_of_valid_passwords_oldplace(values: &[Password]) -> usize {
+    values.iter().filter(|pass| {
+        let count = pass.p.chars().filter(|&c| char::eq(&c, &pass.c)).count();
+        pass.v1 <= count && count <= pass.v2
     }).count()
 }
 
 #[aoc(day2, part2)]
-pub fn num_of_valid_passwords_newplace(values: &[(Policy, String)]) -> usize {
-    values.iter().filter(|(policy, password)| {
-
-        password.chars()
+pub fn num_of_valid_passwords_newplace(values: &[Password]) -> usize {
+    values.iter().filter(|pass| {
+        pass.p.chars()
             .enumerate()
             .filter(|(index, c)| {
-                let i = index +1;
-                if policy.v1.eq(&i) || policy.v2.eq(&i) {
-                    policy.c.eq(c)
+                let i = index + 1;
+                if pass.v1.eq(&i) || pass.v2.eq(&i) {
+                    pass.c.eq(c)
                 } else {
                     false
                 }
             }).count() == 1
-
     }).count()
 }
+
+#[aoc(day2, part1, pest)]
+pub fn num_of_valid_passwords_oldplace_pest(values: &[Password]) -> usize {
+    values.iter().filter(|pass| {
+        let count = pass.p.chars().filter(|&c| char::eq(&c, &pass.c)).count();
+        pass.v1 <= count && count <= pass.v2
+    }).count()
+}
+
+#[aoc(day2, part2, pest)]
+pub fn num_of_valid_passwords_newplace_pest(values: &[Password]) -> usize {
+    values.iter().filter(|pass| {
+        pass.p.chars()
+            .enumerate()
+            .filter(|(index, c)| {
+                let i = index + 1;
+                if pass.v1.eq(&i) || pass.v2.eq(&i) {
+                    pass.c.eq(c)
+                } else {
+                    false
+                }
+            }).count() == 1
+    }).count()
+}
+
